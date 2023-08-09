678d66364c2e13f4c1ec228da6b40453ed92e983
zwithe <zwithe133@gmail.com>
2023-08-09 19:34:33 -0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect} from 'react'
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
+import { DataContext } from './context/DataContext';
 function App() {
   let [message, setMessage] = useState('Seach for music')
   let [search, setSearch] = useState('')
@@ -29,7 +30,9 @@ function App() {
     <div className="App">
       <SearchBar handleSearch={handleSearch}/>
       {message}
-      <Gallery data={data}/>
+      <DataContext.Provider value={data}>
+      <Gallery/>
+      </DataContext.Provider>
     </div>
   );
 }
