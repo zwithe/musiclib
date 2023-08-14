91d7296f0a10331d412d2929a7021f09b42d8c4d
zwithe <zwithe133@gmail.com>
2023-08-14 12:32:15 -0400

    finishing touches

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,17 +1,49 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-
+import { useNavigate } from 'react-router-dom'
 function AlbumView() {
     const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
+    const navigate = useNavigate()
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+     const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            
+            {renderSongs}
+            {navButtons()}
         </div>
     )
 }
 
 
+
 export default AlbumView
+
+
